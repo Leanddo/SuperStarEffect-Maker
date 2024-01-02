d645507817efa84a12a1e7494d644f25ae954672
Leandro Amaral <138171959+Leanddo@users.noreply.github.com>
2024-01-02 15:39:32 -0100

    Mario theme version

--- script.js
+++ script.js
@@ -11,15 +11,52 @@ function velocity() {
   root.style.setProperty("--change", 26 - slider + "s");
 }
 
-let inputs = [];
-
-for (let i = 1; i <= 10; i++) {
-  inputs.push(document.getElementById("inp" + i).value);
-}
-
-
-function updatecolor(color,variable) {
-  root.style.setProperty(color,variable);
+function updatecolor(color, variable) {
+  root.style.setProperty(color, variable);
   console.log(inputs);
 }
 
+var navbarOpen = false;
+
+function toggleNav() {
+  let pipe = document.getElementById("pipe");
+  let pconfigs = document.getElementById("pconfigs");
+
+  if (navbarOpen) {
+    pipe.style.top = "-610px";
+  } else {
+    pipe.style.top = "0px";
+  }
+  navbarOpen = !navbarOpen;
+}
+
+let starbutton = document.getElementById("secret");
+var i = 0;
+
+starbutton.onclick = function () {
+  i++;
+
+  if (i == 3) {
+    document.getElementById("musicblock").style.display = "block";
+    document.getElementById("player").play();
+  } else if (i == 6) {
+    document.getElementById("musicblock").style.display = "none";
+    document.getElementById("player").pause();
+  }
+};
+
+let audio = false;
+
+function offaudio() {
+  let media = document.getElementById("player");
+  let musicblock = document.getElementById("musicblockimg");
+  
+  if (audio) {
+    media.pause();
+    musicblock.src = "img/musicblockoff.svg";
+  } else {
+    media.play();
+    musicblock.src = "img/musicblock.svg";
+  }
+  audio = !audio;
+}
